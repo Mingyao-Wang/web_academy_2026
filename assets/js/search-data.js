0e519c78f3dc5f946cc4d98db32d22618097bc2b
Mingyao-Wang <128391042+Mingyao-Wang@users.noreply.github.com>
2026-01-01 23:44:12 +0000

    Deploying to gh-pages from @ Mingyao-Wang/web_academy_2026@11bb7eba7c8cfd12a1b27c217cbd0a30729bb488 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "The full CV is available via the PDF button in the top-right corner.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
