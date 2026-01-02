5813def8d8bfd5b3c2f3410b3dcca4029b68e342
MYW <128391042+Mingyao-Wang@users.noreply.github.com>
2026-01-02 09:48:08 +0900

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -256,7 +256,7 @@ let transTheme = () => {
 let determineThemeSetting = () => {
   let themeSetting = localStorage.getItem("theme");
   if (themeSetting != "dark" && themeSetting != "light" && themeSetting != "system") {
-    themeSetting = "system";
+    themeSetting = "light";  // ← 改成 light
   }
   return themeSetting;
 };
