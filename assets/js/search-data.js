78162545e70d7a76a64d852f56386c275c86a68f
Mingyao-Wang <128391042+Mingyao-Wang@users.noreply.github.com>
2026-09-17 11:09:40 +0000

    Deploying to gh-pages from @ Mingyao-Wang/web_academy_2026@3662eb8f35757f9d27a251416813ee1726cacb2c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "The full CV is available via the PDF button in the top-right corner.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "Teaching Experience",
